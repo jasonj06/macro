318b156cbb5b59b5d62072113bbc7042be560217
Jason <jasonjokumsen177@email.com>
2024-06-20 23:03:15 +0200

    Add mouse coordinates selection

--- newmultiac.py
+++ newmultiac.py
@@ -2,9 +2,23 @@ import pyautogui
 from time import sleep, time
 import gooeypie as gp
 
-def startmacro():
+mouse_pos = []
 
-    return
+def startmacro(event):
+    global mouse_pos
+    monitor = int(mon_sel_dd.selected_index)
+    if monitor == 0:
+        mouse_pos.append((863, 260))
+        mouse_pos.append((1083, 264))
+        mouse_pos.append((1075, 755))
+        mouse_pos.append((822, 759))
+
+    elif monitor == 2:
+        mouse_pos.append((2717, 312))
+        mouse_pos.append((3003, 313))
+        mouse_pos.append((2962, 772))
+        mouse_pos.append((2753, 785))
+
 
 app = gp.GooeyPieApp("Multiautoclicker")
 
@@ -15,19 +29,19 @@ header_lbl.font_size = 20
 
 mon_sel_lbl = gp.Label(app, "Monitor selection:")
 mon_sel_dd = gp.Dropdown(app, ["Monitor 1", "Monitor 2"])
-mon_sel_dd.selected_index = 0
 
 interval_lbl = gp.Label(app, "Time interval in seconds:")
-interval_inp = gp.Input(app)
+interval_inp = gp.Number(app, 5, 1200)
 
 win_layout_lbl = gp.Label(app, "Windows layout:")
 win_layout_dd = gp.Dropdown(app, ["1 window", "2 windows", "3 windows", "4 windows"])
 win_layout_dd.selected_index = 3
 
-start_btn = gp.Button(app, "Start macro", None)
+runtime_lbl = gp.Label(app, "Runtime:")
+
+start_btn = gp.Button(app, "Start macro", startmacro)
 end_btn = gp.Button(app, "End macro", None)
 
-runtime_lbl = gp.Label(app, "Runtime:")
 #####################################
 
 app.set_grid(7, 3)
